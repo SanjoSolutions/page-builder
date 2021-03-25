fe89798c7eda5fd538a279295414ec52f2d74165
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-03-25 21:15:12 +0100

    Saving and sidebar

---
+++ components.js
@@ -0,0 +1,7 @@
+import { Component } from './Component.js'
+import { Component2 } from './Component2.js'
+import { ComponentText } from './ComponentText.js'
+
+customElements.define('page-builder-component', Component)
+customElements.define('page-builder-component2', Component2)
+customElements.define('page-builder-component-text', ComponentText)
